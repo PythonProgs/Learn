2e748be6a57903c2145eab047cc967f83b83a51d
Cipta Adinagara <ciptaadinagara@gmail.com>
2022-02-17 15:12:35 +0700

    Python

---
+++ Gempa Main/Gempa.py
@@ -0,0 +1,11 @@
+def ekstraksi_data():
+    hasil=dict
+    hasil['tanggal']='24 Agustus 2021'
+    hasil['waktu']='14:32'
+    return hasil
+
+def tampilkan_data(result):
+    print('Gempa terakhir berdasarkan BMKG')
+    print(f"Tanggal: {result['tanggal']}")
+    print(f"Waktu: {result['waktu']}")
+
