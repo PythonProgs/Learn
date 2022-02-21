c201edf9000aa92fc5d93bcd36454fdbb3733288
Cipta Adinagara <ciptaadinagara@gmail.com>
2022-02-21 16:54:13 +0700

    Python

---
+++ OOP/__init__.py
@@ -0,0 +1,7 @@
+class Hero:
+
+    def __init__(self, inputname):
+        self.name = inputname
+
+hero1 = Hero("Sniper")
+print(hero1.name)
--- Test.py
+++
@@ -1,5 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-content=requests.get('https://bmkg.go.id')
-print(content.status_code)
